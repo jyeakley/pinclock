63c88026b89cd79ef29133f2b811ff0f48685471
Justin Yeakley <justin@genomoncology.com>
2023-05-20 19:16:57 -0400

    Bug Fix for video length

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -9,7 +9,11 @@ export async function fetchVideos(folders = null) {
 
     if (response.ok) {
         const files = await response.json();
-        videoFiles.set(files);
+        if (files.length === 0){
+            fetchVideos();
+        }else{
+            videoFiles.set(files);
+        }
     } else {
         console.error("Error fetching video files");
     }
