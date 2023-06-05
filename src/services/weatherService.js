b7baaad5a6b42f3bbc1410f245df7aef6d2db40a
Justin Yeakley <justin@genomoncology.com>
2023-06-05 19:30:57 -0400

    Weather bug fix

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -6,8 +6,10 @@ export async function fetchWeatherData(api_key, zipCode) {
             return await response.json();
         } else {
             console.error('Failed to fetch weather data:', response.status, response.statusText);
+            return null; // Return null when the request fails
         }
     } catch (error) {
         console.error('Error fetching weather data:', error);
+        return null; // Return null when the request fails
     }
 }
