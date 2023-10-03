c31c230a998629c59c2132a358f4757b5da528a9
Justin Yeakley <justin@genomoncology.com>
2023-10-03 15:38:17 -0400

    inline play

--- src/components/VideoPlayer.svelte
+++ src/components/VideoPlayer.svelte
@@ -170,7 +170,7 @@
             src="/videos/{videos[videoIndex]}"
             muted
             preload="metadata"
-            controls playsinline
+            playsinline
     ></video>
 {/if}
 <div class="black-background" class:show="{$showBlackBackground}"></div>
